6a908a2dfc830ef6d172096249362db517e98d99
m.semenov <m.semenov@corp.mail.ru>
2021-01-26 14:07:44 +0300

    split files

---
+++ slapp/commands.py
@@ -0,0 +1,57 @@
+import typer
+import git
+
+from .main import app
+from .config import get_config, set_config
+from .utils import (
+    parse_changelogs_from_repo,
+    write_changelogs_to_file,
+    echo_changelog,
+)
+
+
+@app.command()
+def release(version: str):
+    config = get_config()
+    if not config:
+        return
+
+    try:
+        repo = git.Repo(config['repo_directory'].get())
+    except git.NoSuchPathError:
+        typer.echo(typer.style('No git repository found.', fg=typer.colors.RED))
+        return
+
+    release_branch = config['release_branch'].get()
+    if repo.active_branch.name != release_branch:
+        typer.echo(typer.style(
+            f'Current branch is {repo.active_branch.name}.\nSwitch to {release_branch} branch first.',
+            fg=typer.colors.RED
+        ))
+        return
+
+    changelogs = parse_changelogs_from_repo(repo)
+    changelog_file = config['changelog_file'].get()
+    write_changelogs_to_file(version, changelogs)
+
+    if changelogs:
+        echo_changelog(version, changelogs)
+    else:
+        typer.echo(typer.style('No changelog provided.', fg=typer.colors.YELLOW))
+
+    try:
+        repo.git.add(changelog_file)
+        repo.index.commit(f'Update {changelog_file}')
+        repo.remotes.origin.push()
+
+    except Exception as e:
+        typer.echo(typer.style('Some error occured while pushing the changelog', fg=typer.colors.RED))
+        typer.echo(e)
+
+    new_tag = repo.create_tag(version, message=version)
+    repo.remotes.origin.push(new_tag)
+
+
+@app.command()
+def config_init():
+    set_config()
---
+++ slapp/utils.py
@@ -0,0 +1,65 @@
+import os
+import git
+import marko
+import typer
+
+
+def extract_changelogs_from_commit(message: str or None,):
+    changelogs = []
+    if not message or '*' not in message:
+        return changelogs
+
+    for item in marko.parse(message).children:
+        if isinstance(item, marko.block.List):
+            changelogs.extend(
+                [
+                    child.children[0].children[0].children
+                    for child in item.children
+                ]
+            )
+
+    return changelogs
+
+
+def parse_changelogs_from_repo(repo: git.Repo) -> list:
+    changelogs = []
+    if repo.tags:
+        last_tag_commit_hexsha = repo.tags[-1].commit.hexsha
+        for commit in repo.iter_commits():
+            if commit.hexsha == last_tag_commit_hexsha:
+                break
+            changelogs.extend(extract_changelogs_from_commit(commit.message))
+    else:
+        for commit in repo.iter_commits():
+            changelogs.extend(extract_changelogs_from_commit(commit.message))
+
+    return changelogs
+
+
+def echo_changelog(version, changelog):
+    typer.echo(typer.style(
+        f'{version} changelog:',
+        fg=typer.colors.BLUE,
+        bold=True
+    ))
+    typer.echo('\n'.join(changelog))
+
+
+def write_changelogs_to_file(
+    version, changelogs, changelog_file,
+    divider: 'str' = '='*10, bullet: str = '*'
+):
+    def touchopen(filename, *args, **kwargs):
+        if not os.path.isfile(filename):
+            open(filename, "a").close()
+        return open(filename, *args, **kwargs)
+
+    rendered_changelog = "\n".join(
+        [f'{bullet} {item}' for item in changelogs]
+    )
+
+    with touchopen(changelog_file, 'r+') as f:
+        content = f.read()
+        f.seek(0)
+        f.write(f'{version}\n{divider}\n{rendered_changelog}\n\n{content}')
+        f.truncate()
