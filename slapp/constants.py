f643cebe695890bd8ace03bcab3747affe6529d2
m.semenov <m.semenov@corp.mail.ru>
2021-01-31 19:46:55 +0300

    * added automatic version incrementation

---
+++ slapp/constants.py
@@ -0,0 +1,10 @@
+CONFIG_FILE = 'slapp.yml'
+
+DEFAULT_CONFIG = {
+    'repo_directory': '.git',
+    'release_branch': 'main',
+    'changelog_file': 'CHANGELOG.md',
+    'bullet': '*',
+}
+
+VERSION_TYPES = ('major', 'minor', 'patch')
